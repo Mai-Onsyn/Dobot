761430f1759f12f0ffc5f974f8f80152f0b81947
Mai-Onsyn <254454815@qq.com>
2026-04-28 19:24:52 +0800

    add trajectory page

--- composeApp/src/jvmMain/kotlin/cius/mai_onsyn/dobot/gui/content/Page.kt
+++ composeApp/src/jvmMain/kotlin/cius/mai_onsyn/dobot/gui/content/Page.kt
@@ -5,6 +5,7 @@ import cius.mai_onsyn.dobot.gui.content.movement.AdjustPage
 import cius.mai_onsyn.dobot.gui.content.experience.ExperiencePage
 import cius.mai_onsyn.dobot.gui.content.monitor.MonitorPage
 import cius.mai_onsyn.dobot.gui.content.settings.SettingsPage
+import cius.mai_onsyn.dobot.gui.content.trajectory.TrajectoryPage
 import cius.mai_onsyn.dobot.gui.content.welcome.WelcomePage
 
 enum class Page(
@@ -14,6 +15,7 @@ enum class Page(
     WELCOME("Welcome", { WelcomePage() }),
     MONITOR("监控", { MonitorPage() }),
     ADJUST("运动", { AdjustPage() }),
+    TRAJECTORY("轨迹", { TrajectoryPage() }),
     EXPERIENCE("实验", { ExperiencePage() }),
     SETTINGS("设置", { SettingsPage() })
 }
