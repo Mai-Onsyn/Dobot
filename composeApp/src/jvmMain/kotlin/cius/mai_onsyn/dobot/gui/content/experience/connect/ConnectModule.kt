b7359de8d08a9651108a3fc48beb229e372f6895
weilantianhai <672391806@qq.com>
2026-04-29 23:31:18 +0800

    连接状态

--- composeApp/src/jvmMain/kotlin/cius/mai_onsyn/dobot/gui/content/experience/connect/ConnectModule.kt
+++ composeApp/src/jvmMain/kotlin/cius/mai_onsyn/dobot/gui/content/experience/connect/ConnectModule.kt
@@ -1,13 +1,23 @@
 package cius.mai_onsyn.dobot.gui.content.experience.connect
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import cius.mai_onsyn.dobot.gui.ROUND_CORNER_SHAPE
 import cius.mai_onsyn.dobot.gui.util.universal_module.CardBase
 
 @Composable
@@ -17,12 +27,43 @@ fun ConnectModule(
     CardBase(
         modifier = modifier
     ) {
-        Box(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "连接状态",
-                modifier = Modifier.align(Alignment.TopStart),
-                color = MaterialTheme.colorScheme.onSurfaceVariant
+        Column(modifier = Modifier.padding(start = 12.dp
+            ,end = 0.dp, top = 12.dp, bottom = 24.dp
+        )
+            .fillMaxSize()) {
+            Box(modifier = Modifier
+                .padding(4.dp)){
+                Text(
+                    text = "连接状态",
+                    modifier = Modifier,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp)
             )
+            Row(modifier= Modifier.fillMaxSize()
+            ) {
+                Column(modifier = Modifier.weight(1f)
+                    .padding(end = 12.dp)
+                    .fillMaxSize()
+                ) {
+                    DeviceStatusLabel(name = "机械臂",modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    DeviceStatusLabel(name = "灵巧手",modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    DeviceStatusLabel(name = "升降台",modifier = Modifier.weight(1f))
+                }
+                Column(modifier = Modifier.weight(1f)
+                    .padding(end = 12.dp)
+                    .fillMaxSize()
+                ) {
+                    DeviceStatusLabel(name = "蠕动泵",modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    DeviceStatusLabel(name = "抽水泵",modifier = Modifier.weight(1f))
+                    Spacer(modifier = Modifier.height(20.dp))
+                    DeviceStatusLabel(name = "摄像机",modifier = Modifier.weight(1f))
+                }
+            }
         }
     }
 }
---
+++ composeApp/src/jvmMain/kotlin/cius/mai_onsyn/dobot/gui/content/experience/connect/UnitStatus.kt
@@ -0,0 +1,57 @@
+package cius.mai_onsyn.dobot.gui.content.experience.connect
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import cius.mai_onsyn.dobot.gui.ROUND_CORNER_SHAPE
+import cius.mai_onsyn.dobot.gui.util.background
+
+@Composable
+fun DeviceStatusLabel(name: String, isConnected: Boolean=true,modifier: Modifier=Modifier) {
+    val statusText = if (isConnected) "已连接" else "未连接"
+    val statusColor = if (isConnected) Color(0xFF2E7D32) else Color.Red
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                shape = ROUND_CORNER_SHAPE
+            )
+            .padding(12.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row{
+            Text(text="$name",
+                modifier = Modifier.width(60.dp),
+                color=MaterialTheme.colorScheme.onSurface)
+            Spacer(modifier = Modifier.width(30.dp))
+            Box(
+                modifier = Modifier
+                    .size(10.dp)
+                    .offset(y=5.dp)
+                    .background(statusColor, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+            Text(text=statusText,color=statusColor)
+        }
+
+    }
+}
